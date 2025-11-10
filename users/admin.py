ae7143aaca19415008cd73f65577d41f917d4a73
Iconia7 <mwanginewton239@gmail.com>
2025-11-10 12:00:27 +0300

    users

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,26 @@
-# users/admin.py
-
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
 
-# Get your custom User model
 User = get_user_model()
 
-# Register your User model with the UserAdmin class
-admin.site.register(User, UserAdmin)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+    list_display = ('email', 'name', 'adm_no', 'phone_number', 'is_staff', 'is_superuser')
+    search_fields = ('email', 'name', 'adm_no', 'phone_number')
+    ordering = ('email',)
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name', 'adm_no', 'phone_number', 'koin_score')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'is_staff', 'is_superuser'),
+        }),
+    )
